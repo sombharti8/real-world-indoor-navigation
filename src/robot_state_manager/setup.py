32d72f9995efc34165d73f063ffe391b51d626da
Som Bharti <131845457+sombharti8@users.noreply.github.com>
2026-08-27 16:27:18 +0530

    Fix ROS 2 package data installation

--- src/robot_state_manager/setup.py
+++ src/robot_state_manager/setup.py
@@ -1,8 +1,24 @@
 from setuptools import setup
+from glob import glob
+import os
+
 package_name = 'robot_state_manager'
+
 setup(
-    name=package_name, version='0.1.0', packages=[package_name],
-    data_files=[('share/ament_index/resource_index/packages',['resource/'+package_name]),('share/'+package_name,['package.xml'])],
-    install_requires=['setuptools'], zip_safe=True,
-    entry_points={'console_scripts':['robot_state_manager = robot_state_manager.robot_state_manager:main']},
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'robot_state_manager = robot_state_manager.robot_state_manager:main',
+        ],
+    },
 )
